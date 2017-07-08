9555fb9538e04bb8a8c84121bdbbd2ca496973f6
Elias Storme <elias.storme@gmail.com>
2017-07-08 15:17:12 +0200

    Created holder interface
    Interface for specifying how items should deal with their holder.

---
+++ src/rpg/inventory/Holder.java
@@ -0,0 +1,13 @@
+package rpg.inventory;
+
+import rpg.Mobile;
+
+/**
+ * An interface specifying how an item should handle its holder.
+ * @author elias
+ *
+ */
+public interface Holder {
+	
+	public Mobile getHolder();
+}
