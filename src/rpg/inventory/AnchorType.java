7d9f37cb4f3da3c32f0cfd466b9e4acbb365c129
Elias Storme <elias.storme@gmail.com>
2017-07-08 18:16:46 +0200

    Worked on anchorpoint
    Created anchortype enum and completed its implementation.
    Started constructing Anchorpoint class

---
+++ src/rpg/inventory/AnchorType.java
@@ -0,0 +1,17 @@
+package rpg.inventory;
+
+/**
+ * Created by elias on 08/07/17.
+ */
+public enum AnchorType {
+    //Left hand anchorpoint
+    lhand(),
+    //Right hand anchorpoint
+    rhand(),
+    //Back anchorpoint
+    back(),
+    //Body anchorpoint
+    body(),
+    //Belt anchorpoint
+    belt()
+}
--- src/rpg/inventory/Anchorpoint.java
+++ src/rpg/inventory/Anchorpoint.java
@@ -1,30 +1,95 @@
 package rpg.inventory;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Raw;
+import rpg.Mobile;
 import rpg.inventory.Container;
 
-public class Anchorpoint extends Container {
+/**
+ * A class of Anchorpoints to which mobiles can attach Items.
+ *
+ * @invar The Item currently being stored in the anchorpoint
+ *        has to be of the right type as dictated by the
+ *        anchorpoint type.
+ *      | canHaveAsItem(this.content)
+ */
+public class Anchorpoint implements Holder {
 
-	public Anchorpoint(int value) {
-		super(value);
-		// TODO Auto-generated constructor stub
+
+    /**
+     * Creates a new anchorpoint of given type with given holder.
+     * @param holder
+     */
+	public Anchorpoint(Mobile holder, AnchorType type) {
+		this.holder = holder;
+		this.type = type;
 	}
 
-	@Override
-	public boolean canHaveAsContent(Item item) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    /*****************************
+     * Holder
+     *****************************/
 
-	@Override
-	public int getValue() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Basic
+    public Mobile getHolder({
+        return this.holder;
+    }
 
-	@Override
-	public boolean canHaveAsValue() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    /**
+     * Variable that stores a reference to the holder of this anchorpoint.
+     *
+     * @note An anchorpoint is a container inherent to the holder so it cannot
+     * be transferred between mobiles. As a result the holder is set at
+     * initialisation.
+     */
+    private final Mobile holder;
+
+    /*****************************
+     * Type
+     *****************************/
+
+    @Basic
+    public AnchorType getType() {
+        return type;
+    }
+
+    public boolean canHaveAsItem(Item item){
+
+    }
+
+    /**
+     * Variable that determines what type this anchorpoint is.
+     *
+     * @note The type of anchorpoint determines what
+     */
+    private final AnchorType type;
+
+    /*****************************
+    * Type
+    *****************************/
+
+
+    @Raw @Basic
+    public Item getContent() {
+        return content;
+    }
+
+    @Raw @Basic
+    private void setContent(Item content) {
+        this.content = content;
+    }
+
+    /**
+     * Drops the item currently being held in this anchorpoint.
+     *
+     * @effect The item that was being stored in the anchorpoint has
+     *         its parent set to null.
+     *       |
+     */
+    public void dropContent(){
+
+    }
+
+
+    private Item content = null;
 
 }
