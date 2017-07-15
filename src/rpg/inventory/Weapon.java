50ea50f10bb15d52520009a6027314a6464d5b25
stormeuh <elias.storme@gmail.com>
2017-07-15 21:21:18 +0200

    Weapon almost finished

--- src/rpg/inventory/Weapon.java
+++ src/rpg/inventory/Weapon.java
@@ -1,40 +1,171 @@
 package rpg.inventory;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Raw;
 import rpg.IDGeneration.IDGenerator;
 import rpg.IDGeneration.WeaponIDGenerator;
 import rpg.Mobile;
 import rpg.value.Weight;
 
 /**
- * Created by elias on 08/07/17.
+ * A class of weapons usable by mobiles.
+ *
+ * @invar The identifier for an instance of Weapon is an even, positive integer
+ * divisible by three.
+ * | identifier mod 2 == 0 && identifier mod 3 == 0 && identifier > 0
+ * @invar The damage of the weapon must be effective.
+ * | canHaveAsDamage(dmg)
+ *
+ * @author Elias Storme
+ * @version 2.0
  */
 public class Weapon extends Item implements hasParent{
 
-    public Weapon(int value, long identifier) {
-        super(value, Weight.kg_0);
-    }
 
-    public int getValue() {
-        return 0;
-    }
+     //TODO make it so weapons destroy themselves when trying to add to backpack but cannot carry under any circumstance, see 3.5
 
-    public boolean canHaveAsValue(int value) {
-        return false;
+
+    /*****************************
+     * Constants
+     *****************************/
+
+    /**
+     * Constant specifying the maximum value the damage of a weapon may be.
+     */
+    private static int MAX_DMG = 100;
+
+    /**
+     * Constant specifying how many ducats a unit of damage is worth.
+     */
+    private static int VALUE_PER_DMG = 2;
+
+    /*****************************
+     * 3.0: Constructors
+     *****************************/
+
+    /**
+     * Creates a new weapon with the given
+     *
+     * @param value
+     *        Value of the weapon.
+     * @param weight
+     *        Weight of the weapon.
+     * @param dmg
+     *        Damage value for this weapon.
+     *
+     * @pre The given damage must be an effective value.
+     * | canHaveAsDamage(dmg)
+     */
+    public Weapon(int value, Weight weight, int dmg) {
+        super(value, weight);
+        setDamage(dmg);
     }
 
     /*****************************
-     * Identification
+     * 3.1: Identification
      *****************************/
 
-    @Override
+    /**
+     * Getter for the IDGenerator for the weapon class.
+     */
+    @Override @Basic @Raw
     IDGenerator getIDGenerator() {
         return idGen;
     }
 
+    /**
+     * IDGenerator for the weapon class.
+     * Generates multiples of six so the ID of the weapon is always an even, positive
+     * integer divisible by three.
+     */
     private static WeaponIDGenerator idGen = new WeaponIDGenerator();
 
     /*****************************
-     * Holder
+     * 3.3: Damage
+     *****************************/
+
+    /**
+     * Checks if the given value is a valid damage value.
+     * @param dmg
+     *        Value to be checked.
+     * @return True if the given value lies within the range [1,100]
+     * and it is divisible by 7
+     * | return dmg > 0 && dmg <= MAX_DMG && (dmg mod 7) == 0
+     */
+    @Immutable
+    public boolean canHaveAsDamage(int dmg){
+        return dmg > 0
+                && dmg <= getMaxDamage()
+                && (dmg % 7) == 0;
+    }
+
+    /**
+     * Sets the damage of this weapon to the given value.
+     * @param dmg
+     *        Value to be set.
+     * @pre The given damage must be an effective value.
+     * | canHaveAsDamage(dmg)
+     */
+    @Basic @Raw
+    public void setDamage(int dmg){
+        this.dmg = dmg;
+    }
+
+    /**
+     * Getter for the damage of this weapon.
+     */
+    @Basic @Raw
+    public int getDamage(){
+        return this.dmg;
+    }
+
+    /**
+     * Getter for the maximum damage.
+     * May be modified later to return a dynamic result.
+     */
+    @Basic @Raw @Immutable
+    private int getMaxDamage(){
+        return MAX_DMG;
+    }
+
+    /**
+     * Variable for storing the damage this weapon can do.
+     */
+    private int dmg = 0;
+
+    /*****************************
+     * 3.4: Value
+     *****************************/
+
+    /**
+     * Calculates the value of this weapon.
+     *
+     * @return Returns the product of the damage this weapon can do
+     * and the value per damage.
+     * | return getDamage * getValuePerDamage
+     */
+    @Override
+    public int getValue() {
+        return getDamage() * getValuePerDamage();
+    }
+
+    /**
+     * Gets the amount of ducats a unit of damage is worth.
+     * @return The constant specifying how much a unit of damage is worth.
+     * | return VALUE_PER_DMG
+     */
+    @Basic @Raw @Immutable
+    private int getValuePerDamage(){
+        return VALUE_PER_DMG;
+    }
+
+    public boolean canHaveAsValue(int value){
+        return value > 0 && value >= 200;
+    }
+
+    /*****************************
+     * 3.5: Holder
      *****************************/
 
     /**
@@ -48,6 +179,7 @@ public class Weapon extends Item implements hasParent{
      * holder.
      * | else return parent.getHolder()
      */
+    @Raw
     public Mobile getHolder() {
         if (parent == null) {
             return null;
